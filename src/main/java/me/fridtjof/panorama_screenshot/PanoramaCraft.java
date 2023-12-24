beaa883ef3532aab3ba813a8e5bb5fccb44f9111
Fridtjof_DE <38737231+Fridtjof-DE@users.noreply.github.com>
2023-12-24 17:51:46 +0100

    renaming

--- src/main/java/me/fridtjof/panorama_screenshot/PanoramaCraft.java
+++ src/main/java/me/fridtjof/panorama_screenshot/PanoramaCraft.java
@@ -1,4 +1,4 @@
-package me.fridtjof;
+package me.fridtjof.panorama_screenshot;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.SimpleOption;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
@@ -32,6 +33,7 @@ public class PanoramaCraft implements ModInitializer {
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (panoramaKeyBinding.wasPressed()) {
+
 				client.takePanorama(GAME_DIR, 1024, 1024);
 
 
